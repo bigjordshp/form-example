b5e7248b7b7701ddd80d260e478b69ae9c24a67a
bigjordshp <124464456+bigjordshp@users.noreply.github.com>
2024-01-24 10:25:52 -0500

    Add base content and form validation

---
+++ js.js
@@ -0,0 +1,13 @@
+// Select the button by its id
+const submitbtn = document.getElementById('submitbtn');
+
+// Add an event listener to the button
+submitbtn.addEventListener('click', function() {
+  // Code to run when the button is clicked
+  alert('Button clicked!');
+  console.log(password.textContent)
+  // Add your custom logic here
+});
+
+const password = document.getElementById("password")
+console.log(password.textContent)
